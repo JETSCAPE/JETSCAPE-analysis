6003d51556010283e411e3569d1b504af14e7425
Raymond Ehlers <raymond.ehlers@gmail.com>
2026-05-01 04:39:29 +0000

    Improvements to observable name encoding

--- jetscape_analysis/data_curation/observable.py
+++ jetscape_analysis/data_curation/observable.py
@@ -1370,13 +1370,19 @@ class Observable:
         # -1 removes the experiment name
         return pretty_print_name(self.internal_name_without_experiment)
 
-    def encode_name_for_storing_in_file(self, **parameters_to_encode: ParameterSpec) -> str:
-        # The baseline is that we want to encode the essential parameters, as well as any others
-        # that we might be interested in.
-        # Determine additional parameters to encode (if available)
+    def encode_name_for_storing_in_file(self, *, tag: str = "", **parameters_to_encode: ParameterSpec) -> str:
+        # The baseline that we want to encode the essential parameters.
+        # From there, we have two possible modifications:
+        # 1. Additional parameters that we want to include (if they're available).
         additional_parameters_to_encode = [JetRSpec]
+        # 2. Parameters that we want to exclude (namely, those which we don't want to specify at the observable level)
+        #    For example, we skip the centrality since we want to deal with that binning later.
+        parameters_to_exclude = [CentralitySpec]
+
+        # We need the encoded parameter names to have the right names for comparison
         encoded_parameters = {p.encode_name: p for p in self.parameters()}
         encoded_essential_parameters = {p.encode_name: p for p in self.essential_parameters()}
+
         # Loop over the available parameters to order the additional parameters w.r.t the essential parameters
         # The order matters for the encoding
         to_encode = {}
@@ -1384,18 +1390,31 @@ class Observable:
             # Only include them if they're in the essential parameters, or we've specifically requested for them
             if not (encoded_name in encoded_essential_parameters or param_specs.spec_type in additional_parameters_to_encode):
                 continue
+            # And also skip those that we've excluded
+            if param_specs.spec_type in parameters_to_exclude:
+                continue
+
             try:
-                to_encode[encoded_name] = parameters_to_encode[encoded_name]
+                to_encode[encoded_name] = parameters_to_encode.pop(encoded_name)
             except KeyError:
                 msg = f"Expected to find {encoded_name}, but was not provided in {parameters_to_encode}. Please add it"
                 raise KeyError(msg)
 
-        # TODO(RJE): Aiming for something like:
+        if parameters_to_encode:
+            logger.warning(f"Provided {parameters_to_encode=}, but they're not needed to encode the name")
+
+        # Aiming for something like:
         # f"inclusive_chjet_ktg_alice_R{jetR}_zcut{zcut}_beta{beta}{jet_collection_label}"
         # TODO(RJE): So I need to encode the jet_R, the grooming setting, and the jet_collection_label
+        # Or for tg:
+        # inclusive_chjet_tg_alice_R0.2_zcut0.2_beta0
         base_name = f"{self.observable_class}_{self.name}"
         for k, v in to_encode.items():
             base_name += f"_{k}_{v.encode()}"
+        # And then include if specified.
+        if tag:
+            base_name += f"_{tag}"
+
         # inclusive_chjet_ktg_alice_centrality_0_10_jet_R_0.2_jet_grooming_settings_DyG_a_1.0
 
         return base_name
